da2ef55702db0b7e73568c37fdedd72747e57785
Marvssss-codes <marvellous.o.akinbola@gmail.com>
2025-10-24 02:09:00 +0100

    chore(ts): add global asset module declarations + nativewind ref

---
+++ global.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="nativewind/types" />
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.webp";
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
